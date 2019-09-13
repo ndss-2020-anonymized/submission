c613920d7056ed58794a6c93770c2d931025b3e8
Anonym Anonym <anonym.anonym.com>
2019-09-13 18:30:32 +0200

    Submission

---
+++ dac/auditing_test.go
@@ -0,0 +1,93 @@
+package dac
+
+import (
+	"testing"
+
+	"gotest.tools/assert"
+
+	"github.com/ndss-2020-anonymized/fabric-amcl/amcl"
+	"github.com/ndss-2020-anonymized/fabric-amcl/amcl/FP256BN"
+)
+
+// Tests
+func TestAuditingHappyPath(t *testing.T) {
+	prg := amcl.NewRAND()
+
+	prg.Clean()
+	prg.Seed(1, []byte{SEED})
+
+	h := FP256BN.ECP_generator().Mul(FP256BN.Randomnum(FP256BN.NewBIGints(FP256BN.CURVE_Order), prg))
+
+	userSk, userPk := GenerateKeys(prg, 1)
+	auditSk, auditPk := GenerateKeys(prg, 1)
+
+	encryption, r := AuditingEncrypt(prg, auditPk, userPk)
+
+	assert.Check(t, pointEqual(encryption.AuditingDecrypt(auditSk), userPk))
+
+	skNym, pkNym := GenerateNymKeys(prg, userSk, h)
+
+	proof := AuditingProve(prg, encryption, userPk, userSk, pkNym, skNym, auditPk, r, h)
+
+	verificationResult := proof.Verify(encryption, userPk, pkNym, auditPk, h)
+
+	assert.Check(t, verificationResult)
+}
+
+// Benchmarks
+
+func BenchmarkAuditingEncrypt(b *testing.B) {
+	prg := amcl.NewRAND()
+
+	prg.Clean()
+	prg.Seed(1, []byte{SEED})
+
+	_, userPk := GenerateKeys(prg, 1)
+	_, auditPk := GenerateKeys(prg, 1)
+
+	for n := 0; n < b.N; n++ {
+		AuditingEncrypt(prg, auditPk, userPk)
+	}
+}
+
+func BenchmarkAuditingProve(b *testing.B) {
+	prg := amcl.NewRAND()
+
+	prg.Clean()
+	prg.Seed(1, []byte{SEED})
+
+	h := FP256BN.ECP_generator().Mul(FP256BN.Randomnum(FP256BN.NewBIGints(FP256BN.CURVE_Order), prg))
+
+	userSk, userPk := GenerateKeys(prg, 1)
+	_, auditPk := GenerateKeys(prg, 1)
+
+	encryption, r := AuditingEncrypt(prg, auditPk, userPk)
+
+	skNym, pkNym := GenerateNymKeys(prg, userSk, h)
+
+	for n := 0; n < b.N; n++ {
+		AuditingProve(prg, encryption, userPk, userSk, pkNym, skNym, auditPk, r, h)
+	}
+}
+
+func BenchmarkAuditingVerify(b *testing.B) {
+	prg := amcl.NewRAND()
+
+	prg.Clean()
+	prg.Seed(1, []byte{SEED})
+
+	h := FP256BN.ECP_generator().Mul(FP256BN.Randomnum(FP256BN.NewBIGints(FP256BN.CURVE_Order), prg))
+
+	userSk, userPk := GenerateKeys(prg, 1)
+	_, auditPk := GenerateKeys(prg, 1)
+
+	encryption, r := AuditingEncrypt(prg, auditPk, userPk)
+
+	skNym, pkNym := GenerateNymKeys(prg, userSk, h)
+
+	proof := AuditingProve(prg, encryption, userPk, userSk, pkNym, skNym, auditPk, r, h)
+
+	for n := 0; n < b.N; n++ {
+		proof.Verify(encryption, userPk, pkNym, auditPk, h)
+	}
+}
---
+++ dac/revocation_test.go
@@ -0,0 +1,126 @@
+package dac
+
+import (
+	"testing"
+
+	"gotest.tools/assert"
+
+	"github.com/ndss-2020-anonymized/fabric-amcl/amcl"
+	"github.com/ndss-2020-anonymized/fabric-amcl/amcl/FP256BN"
+)
+
+// Tests
+func TestRevocationHappyPath(t *testing.T) {
+	g2 := FP256BN.ECP2_generator()
+	const YsNum = 10
+
+	prg := amcl.NewRAND()
+
+	prg.Clean()
+	prg.Seed(1, []byte{SEED})
+
+	h := FP256BN.ECP_generator().Mul(FP256BN.Randomnum(FP256BN.NewBIGints(FP256BN.CURVE_Order), prg))
+
+	epoch := FP256BN.NewBIGint(0x13)
+
+	userSk, userPk := GenerateKeys(prg, 0)
+
+	ys := GenerateYs(false, YsNum, prg)
+	groth := MakeGroth(prg, false, ys)
+
+	revokeSk, revokePk := groth.Generate()
+
+	signature := SignNonRevoke(prg, revokeSk, userPk, epoch, ys)
+
+	assert.Check(t, groth.Verify(revokePk, signature, []interface{}{userPk, g2.Mul(epoch)}))
+
+	skNym, pkNym := GenerateNymKeys(prg, userSk, h)
+
+	proof := RevocationProve(prg, signature, userSk, skNym, epoch, h, ys)
+
+	verificationResult := proof.Verify(pkNym, epoch, h, revokePk, ys)
+
+	assert.Check(t, verificationResult)
+}
+
+// Benchmarks
+
+func BenchmarkRevocationSign(b *testing.B) {
+	const YsNum = 10
+
+	prg := amcl.NewRAND()
+
+	prg.Clean()
+	prg.Seed(1, []byte{SEED})
+
+	epoch := FP256BN.NewBIGint(0x13)
+
+	_, userPk := GenerateKeys(prg, 0)
+
+	ys := GenerateYs(false, YsNum, prg)
+	groth := MakeGroth(prg, false, ys)
+
+	revokeSk, _ := groth.Generate()
+
+	for n := 0; n < b.N; n++ {
+		SignNonRevoke(prg, revokeSk, userPk, epoch, ys)
+	}
+}
+
+func BenchmarkRevocationProve(b *testing.B) {
+	const YsNum = 10
+
+	prg := amcl.NewRAND()
+
+	prg.Clean()
+	prg.Seed(1, []byte{SEED})
+
+	h := FP256BN.ECP_generator().Mul(FP256BN.Randomnum(FP256BN.NewBIGints(FP256BN.CURVE_Order), prg))
+
+	epoch := FP256BN.NewBIGint(0x13)
+
+	userSk, userPk := GenerateKeys(prg, 0)
+
+	ys := GenerateYs(false, YsNum, prg)
+	groth := MakeGroth(prg, false, ys)
+
+	revokeSk, _ := groth.Generate()
+
+	signature := SignNonRevoke(prg, revokeSk, userPk, epoch, ys)
+
+	skNym, _ := GenerateNymKeys(prg, userSk, h)
+
+	for n := 0; n < b.N; n++ {
+		RevocationProve(prg, signature, userSk, skNym, epoch, h, ys)
+	}
+}
+
+func BenchmarkRevocationVerify(b *testing.B) {
+	const YsNum = 10
+
+	prg := amcl.NewRAND()
+
+	prg.Clean()
+	prg.Seed(1, []byte{SEED})
+
+	h := FP256BN.ECP_generator().Mul(FP256BN.Randomnum(FP256BN.NewBIGints(FP256BN.CURVE_Order), prg))
+
+	epoch := FP256BN.NewBIGint(0x13)
+
+	userSk, userPk := GenerateKeys(prg, 0)
+
+	ys := GenerateYs(false, YsNum, prg)
+	groth := MakeGroth(prg, false, ys)
+
+	revokeSk, revokePk := groth.Generate()
+
+	signature := SignNonRevoke(prg, revokeSk, userPk, epoch, ys)
+
+	skNym, pkNym := GenerateNymKeys(prg, userSk, h)
+
+	proof := RevocationProve(prg, signature, userSk, skNym, epoch, h, ys)
+
+	for n := 0; n < b.N; n++ {
+		proof.Verify(pkNym, epoch, h, revokePk, ys)
+	}
+}
